c0e73654cfac81e17e681d73b26b1221a471d8b0
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-12-16 16:11:46 +0800

    Update 02_prepare_package.sh

--- scripts/02_prepare_package.sh
+++ scripts/02_prepare_package.sh
@@ -230,6 +230,7 @@ svn co https://github.com/openwrt/packages/trunk/utils/yq feeds/packages/utils/y
 ln -sf ../../../feeds/packages/utils/yq ./package/feeds/packages/yq
 rm -rf ./feeds/packages/utils/lvm2
 svn co https://github.com/openwrt/packages/trunk/utils/lvm2 feeds/packages/utils/lvm2
+svn co https://github.com/openwrt/openwrt/trunk/package/libs/libselinux package/libs/libselinux
 #补全部分依赖（实际上并不会用到
 svn co https://github.com/openwrt/packages/trunk/libs/nghttp2 feeds/packages/libs/nghttp2
 ln -sf ../../../feeds/packages/libs/nghttp2 ./package/feeds/packages/nghttp2
